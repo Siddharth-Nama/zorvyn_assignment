783566cc989e7f1dda7acbfbcfcce6385b919a12
Siddharth-Nama <siddharthnama2604@gmail.com>
2026-04-02 16:13:47 +0530

    managers for soft deleting records

---
+++ backend/finance/managers.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class SoftDeleteQuerySet(models.QuerySet):
+    def delete(self):
+        return super().update(is_deleted=True)
+
+    def hard_delete(self):
+        return super().delete()
+
+    def alive(self):
+        return self.filter(is_deleted=False)
+
+    def deleted(self):
+        return self.filter(is_deleted=True)
+
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        return SoftDeleteQuerySet(self.model, using=self._db).alive()
+
+    def all_with_deleted(self):
+        return SoftDeleteQuerySet(self.model, using=self._db)
+
+    def deleted_only(self):
+        return SoftDeleteQuerySet(self.model, using=self._db).deleted()
