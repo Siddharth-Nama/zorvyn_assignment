eb34cb1a8501da34bb34ee6ad7a0fc834b50d0a2
Siddharth-Nama <siddharthnama2604@gmail.com>
2026-04-02 16:08:11 +0530

    view for getting my profile info

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
-from rest_framework_simplejwt.views import (
+from .views import (
     TokenObtainPairView,
     TokenRefreshView,
+    UserProfileView,
 )
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('me/', UserProfileView.as_view(), name='user-profile'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSerializer
+from .permissions import IsAuthenticatedAndActive
 
-# Create your views here.
+class UserProfileView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticatedAndActive]
+
+    def get_object(self):
+        return self.request.user
