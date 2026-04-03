3e12aca581dd37f70bf7ebd31c5eae37aa904b90
Siddharth-Nama <siddharthnama2604@gmail.com>
2026-04-03 13:20:34 +0530

    dashboard entry in urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/finance/', include('finance.urls')),
+    path('api/dashboard/', include('dashboard.urls')),
 ]
