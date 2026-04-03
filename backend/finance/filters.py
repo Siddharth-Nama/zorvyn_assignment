f3f1607b24aad9c5041fd63d749440eaa98bfb70
Siddharth-Nama <siddharthnama2604@gmail.com>
2026-04-03 12:58:45 +0530

    filtering and paginating the transaction list

---
+++ backend/finance/filters.py
@@ -0,0 +1,12 @@
+from django_filters import rest_framework as filters
+from .models import Transaction
+
+class TransactionFilter(filters.FilterSet):
+    min_amount = filters.NumberFilter(field_name="amount", lookup_expr='gte')
+    max_amount = filters.NumberFilter(field_name="amount", lookup_expr='lte')
+    start_date = filters.DateFilter(field_name="date", lookup_expr='gte')
+    end_date = filters.DateFilter(field_name="date", lookup_expr='lte')
+
+    class Meta:
+        model = Transaction
+        fields = ['type', 'category', 'date']
